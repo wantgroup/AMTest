b0214d8833177d5a3104a264247732d603bd2a41
1972117027@qq.com <1972117027@qq.com>
2018-06-28 16:45:58 +0800

    提交修改

--- AndProperty/apiutils/setDataCSV.py
+++ AndProperty/apiutils/setDataCSV.py
@@ -1,14 +1,53 @@
 import csv
 import time
 import subprocess
+import os
+import re
+import threading
 
 def LaunchApp():
+    "使用命令启动app"
     cmd = 'adb shell am start -W -n com.youdao.dict/.activity.DictSplashActivity'
     out = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     content = out.stdout.read()
     return content
 
+def LaunchAppCPU():
+    "cpu使用情况命令"
+    cmd = 'adb shell dumpsys cpuinfo | grep com.youdao.dict'
+    out = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    content = out.stdout.read()
+    return content
+
+def monkeyApp():
+    "monkey"
+    cmd = 'adb shell monkey -p com.youdao.dict 200'
+    subprocess.call(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+def setAppCPUSC():
+    "读取曝使用情况信息"
+    content=LaunchAppCPU()
+    o = bytes.decode(content)
+    s = o.split('\n')
+    alldata = [('当前时间', 'cpu使用率')]
+    startTime = ""
+    for item in s:
+        match=re.compile("(.*?)"+"/com.youdao.dict:"+".*?")
+        startTime=re.findall(match,item)
+        startTime=''.join(startTime)
+        match=re.compile("  "+"(.*?)"+"% "+".*?")
+        startTime=re.findall(match,startTime)
+        startTime=''.join(startTime)
+        currentTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        str(startTime)
+        if ''!=startTime:
+            alldata.append((currentTime,startTime+"%"))
+        print(alldata)
+    fileName='cpu.csv'
+    saveDataToCsv(fileName, alldata)
+
 def setAppStartTimeCSV():
+    "读取启动时间"
     content=LaunchApp()
     o = bytes.decode(content)
     s = o.split('\n')
@@ -19,16 +58,21 @@ def setAppStartTimeCSV():
             item = item.replace('\r', ' ')
             startTime = item.split(":")[1]
             startTime = int(startTime.strip())
-
     currentTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     alldata.append((currentTime, startTime))
-    print(alldata)
-    saveDataToCsv(alldata)
+    fileName='some.csv'
+    saveDataToCsv(fileName,alldata)
 
-def saveDataToCsv(alldata):
-    with open('some.csv', 'w',encoding="utf-8") as f:
-        writer = csv.writer(f)
-        writer.writerows(alldata)
+def saveDataToCsv(fileName,alldata):
+    fileCSV=None
+    if(os.path.exists(fileName)):
+        fileCSV = open(fileName, 'a',encoding="utf-8")
+        alldata.pop(0)
+    else:
+        fileCSV = open(fileName, 'w', encoding="utf-8")
+
+    writer = csv.writer(fileCSV)
+    writer.writerows(alldata)
 
 if __name__=="__main__":
-    setAppStartTimeCSV()
+    monkeyApp()
---
+++ AndProperty/apiutils/threadApp.py
@@ -0,0 +1,8 @@
+import csv
+import time
+import subprocess
+import os
+import re
+import threading
+
+class myThread(threading.Thread):
